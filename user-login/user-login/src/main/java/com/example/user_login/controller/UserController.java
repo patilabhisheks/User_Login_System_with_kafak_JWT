491a37f92e88fc0f6409a9f5a2638da31e217a38
Abhishek Patil <patilabhijugul@gmail.com>
2024-12-07 19:23:29 +0530

    Initial commit

---
+++ user-login/user-login/src/main/java/com/example/user_login/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.example.user_login.controller;
+
+import com.example.user_login.model.User;
+import com.example.user_login.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    
+
+//    @PostMapping("/user")
+//    ResponseEntity<User> createUser(@RequestBody User user)
+//    {
+//        userService.
+//    }
+//    @GetMapping("/user")
+//    ResponseEntity<User> getUser(@RequestBody User user)
+//    {
+//        return
+//    }
+}
---
+++ user-login/user-login/src/main/java/com/example/user_login/service/UserService.java
@@ -0,0 +1,18 @@
+package com.example.user_login.service;
+
+import com.example.user_login.model.User;
+import com.example.user_login.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public static void creteUser(User user)
+    {
+                return
+    }
+}
